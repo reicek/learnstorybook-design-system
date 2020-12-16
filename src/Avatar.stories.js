4ca98c2f191e1ece4e7bccb3c17b905de178030e
Cesar Anton Dorantes <reicek@gmail.com>
2020-12-16 12:40:29 -0500

    getting started : review

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -59,3 +59,14 @@ export const Large = (args) => (
     />
   </div>
 );
+
+// New story using controls
+const Template = args => <Avatar {...args} />;
+
+export const Controls = Template.bind({});
+Controls.args = {
+  loading: false,
+  size: 'tiny',
+  username: 'Dominic Nguyen',
+  src: 'https://avatars2.githubusercontent.com/u/263385',
+};
