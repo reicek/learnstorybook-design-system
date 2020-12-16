fa4b88f9ac4cc0a3021f3b40f64cba4ad96460a7
Cesar Anton Dorantes <reicek@gmail.com>
2020-12-16 14:44:37 -0500

    stories docs

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -3,8 +3,11 @@ import React from "react";
 import { Avatar } from "./Avatar";
 
 export default {
-  title: "Design System/Avatar",
+  title: 'Design System/Avatar',
   component: Avatar,
+  parameters: {
+    componentSubtitle: 'Displays an image that represents a user or organization',
+  },
 };
 
 export const Standard = (args) => <Avatar {...args} />;
@@ -25,6 +28,12 @@ export const Sizes = (args) => (
 Sizes.args = {
   username: "Tom Coleman",
   src: "https://avatars2.githubusercontent.com/u/132554",
+};
+Sizes.parameters = {
+  docs: {
+    // The story now contains a description
+    storyDescription: '4 sizes are supported.',
+  },
 };
 
 export const Initials = (args) => (
